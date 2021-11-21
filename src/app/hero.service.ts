26e732a737b1bb256c6ce77bb57b4dcf61118f89
uenobu106 <uenobu106@gmail.com>
2021-11-21 16:17:29 +0900

    toh-pt5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
+
 import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
   constructor(private messageService: MessageService) {}
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // 今のところ、指定された `id` を持つヒーローが常に存在すると仮定します。
+    // エラー処理はチュートリアルの次のステップで追加されます。
+    const hero = HEROES.find((hore) => hore.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
